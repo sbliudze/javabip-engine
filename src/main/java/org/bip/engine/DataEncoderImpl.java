f27d1a35bf66aefa2ef47c131cf0f05314f17940
Anastasia Mavridou <anastasia.mavridou@epfl.ch>
2013-09-05 15:50:18 +0200

    Data encoder implementeation has launched..

--- src/main/java/org/bip/engine/DataEncoderImpl.java
+++ src/main/java/org/bip/engine/DataEncoderImpl.java
@@ -1,5 +1,36 @@
 package org.bip.engine;
 
-public class DataEncoderImpl {
+import java.util.Map;
+
+import net.sf.javabdd.BDD;
+
+import org.bip.api.BIPComponent;
+import org.bip.behaviour.Port;
+
+public class DataEncoderImpl implements DataEncoder{
+
+	@Override
+	public BDD inform(Map<BIPComponent, Port> disabledCombinations) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setBIPCoordinator(BIPCoordinator wrapper) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBehaviourEncoder(BehaviourEncoder behaviourEncoder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEngine(BDDBIPEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
