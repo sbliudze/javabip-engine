e3738ffd7e3fc179000c2bb31484698fcfe5835e
Alina Zolotukhina <alina.zolotukhina@epfl.ch>
2013-09-05 14:38:25 +0200

    data coordinator impl added (with no implementation so far)

---
+++ src/main/java/org/bip/engine/DataCoordinatorImpl.java
@@ -0,0 +1,50 @@
+package org.bip.engine;
+
+import java.util.ArrayList;
+
+import org.bip.api.BIPComponent;
+import org.bip.api.BIPEngine;
+import org.bip.api.Behaviour;
+import org.bip.api.DataCoordinator;
+import org.bip.behaviour.Port;
+import org.bip.glue.BIPGlue;
+
+public class DataCoordinatorImpl implements DataCoordinator{
+
+	@Override
+	public void specifyGlue(BIPGlue glue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void register(BIPComponent component, Behaviour behaviour) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inform(BIPComponent component, String currentState, ArrayList<Port> disabledPorts) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
